b3192353fe641a5555153d7dc8dd7d5a461c70fb
SUJI12345 <47381551+SUJI12345@users.noreply.github.com>
2019-02-06 02:23:40 -0800

    Update oddeven.java

--- oddeven.java
+++ oddeven.java
@@ -1,14 +1,12 @@
-public class Main
+class Main
 {
-
-    public static void main(String[] args) {
-
-        int num;
-        num =5;
-
-        if(num % 2 == 0)
-            System.out.println(num + " is even");
-        else
-            System.out.println(num + " is odd");
-    }
+public static void main(String[] args)
+{
+int num;
+ num =5;
+if(num % 2 == 0)
+ System.out.println(num + " is even");
+ else
+ System.out.println(num + " is odd");
+}
 }

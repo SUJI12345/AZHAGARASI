7dfea9885cf226b21915a5201abf02a3eaacaeae
SUJI12345 <47381551+SUJI12345@users.noreply.github.com>
2019-03-08 11:08:03 +0530

    Update SumOfNNumbers.java

--- SumOfNNumbers.java
+++ SumOfNNumbers.java
@@ -1,14 +1,15 @@
-import java.util.Scanner;
-public class SumOfNNumbers {
-   public static void main(String args[]){
-      int sum = 0;
-      System.out.print("Enter the number value:: ");
-      Scanner sc = new Scanner(System.in);
-      int num = sc.nextInt();
 
-      for (int i = 0; i<num; i++){
-         sum = sum +i;
-      }
-      System.out.println("Sum of numbers : "+sum);
-   }
+import java.util.Scanner;
+class Natural
+{
+	
+	public static void main(String args[])
+	{
+		int s=0;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		s=s+i;
+		System.out.println(s);
+	}
 }

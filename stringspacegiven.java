a0ed1dce15ada59778646d2f04f5dfb9c9a02f10
SUJI12345 <47381551+SUJI12345@users.noreply.github.com>
2019-03-09 12:21:24 +0530

    Create stringspacegiven.java

---
+++ stringspacegiven.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class stringspacegiven 
+
+{
+
+public static void main(String[] args) 
+{
+
+Scanner sc = new Scanner(System.in);
+String str= sc.nextLine();
+String array[]= str.split(" ",0);
+System.out.println((array.length)-1);
+
+}
+}

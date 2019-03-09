85adbe20d199b0d22ca3807810b623f221618ce1
SUJI12345 <47381551+SUJI12345@users.noreply.github.com>
2019-03-09 16:43:58 +0530

    Create arraynumamong.java

---
+++ arraynumamong.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class arraynumamong
+{
+   public static void main(String arg[])
+ {
+     Scanner input=new Scanner(System.in);
+       
+   for(int i=0;i<10;i++)
+        {
+            arrayNum[i]=input.nextInt();
+        }
+        Arrays.sort(arrayNum);
+         
+         
+            System.out.print(arrayNum[0]);
+        
+    }
+}

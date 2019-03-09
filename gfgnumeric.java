f570bfe6a21ee5ce54fdee96efe603bfae30a01f
SUJI12345 <47381551+SUJI12345@users.noreply.github.com>
2019-03-09 12:00:13 +0530

    Create gfgnumeric.java

---
+++ gfgnumeric.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*; 
+class gfgnumeric
+{ 
+public static void main(String[] args) 
+ {
+  Scanner s = new Scanner(System.in);  
+  String number = s.next(); 
+  boolean numeric = true;
+ try 
+ {
+   Double num = Double.parseDouble(number);
+ } 
+   catch (NumberFormatException e) 
+   {
+    numeric = false;
+   }
+if(numeric)
+   {
+          System.out.println("yes");
+          }
+        else
+        {
+            System.out.println("No");
+        }
+    }
+} 

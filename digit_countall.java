39bf91cbfd0d79cc751a02c6a07c66a9ca79901a
SUJI12345 <47381551+SUJI12345@users.noreply.github.com>
2019-03-09 15:53:28 +0530

    Create  digit_countall.java

---
+++ digit_countall.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+ class digit_countall
+{
+  public static void main(String args[]){
+  int m, n,number=0;
+  Scanner s = new Scanner(System.in);
+  m = s.nextInt();
+  while(m > 0){
+  n = m % 10;
+  m = m / 10;
+  number+=n;           
+}    
+   System.out.print(number);
+        
+    }
+}

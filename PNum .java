b1ac2d8df76833ba3920c55b0cf5e9c199f29dcf
SUJI12345 <47381551+SUJI12345@users.noreply.github.com>
2019-03-09 10:06:16 +0530

    Create  PNum .java

---
+++ PNum .java
@@ -0,0 +1,10 @@
+import java.util.*;
+class PNum {
+   public static void main(String args[])
+   {
+      Scanner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      System.out.println(Math.round(Math.pow(a,b)));
+   }
+}

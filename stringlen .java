cfa9e3bb980a35f36ce3a636fef2539a9e15a4ff
SUJI12345 <47381551+SUJI12345@users.noreply.github.com>
2019-03-09 12:12:25 +0530

    Create stringlen .java

---
+++ stringlen .java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class stringlen 
+{
+public static void main(String[] args) 
+{
+Scanner sc = new Scanner(System.in);
+String str= sc.nextLine();
+int count=0;
+String array[]= str.split(" ",0);
+for(int i=0;i<array.length;i++){count+=array[i].length();}
+System.out.println(count);
+}
+}
+ © 2019 GitHub, Inc.
+ Terms
+ Privacy
+ Security
+ Status
+ Help

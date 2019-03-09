7d92db7ed406cbaac81ed8af7e3ce350eb1045f8
SUJI12345 <47381551+SUJI12345@users.noreply.github.com>
2019-03-09 10:50:03 +0530

    Create Ideonev .java

---
+++ Ideonev .java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideonev 
+{
+public static void main (String[] args) throws java.lang.Exception
+{
+int i,a,b;
+Scanner sc=new Scanner(System.in);
+a=sc.nextInt();
+b=sc.nextInt();
+for(i=a+1;i<b;i++)
+if(i%2==0)
+{
+System.out.println(i);
+		}
+	}
+}

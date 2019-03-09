1c93d2eda03e5ca0340b5b6c253b6f70957d63ab
SUJI12345 <47381551+SUJI12345@users.noreply.github.com>
2019-03-09 12:06:08 +0530

    Create  Hourminute.java

---
+++ Hourminute.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Hourminute
+{
+  public static void main(String[] args){
+  int minute;
+  Scanner s = new Scanner(System.in); 
+   minute= s.nextInt();
+   System.out.println((minute/60)+" "+(minute%60));
+ }
+}
